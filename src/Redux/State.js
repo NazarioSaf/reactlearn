990c90c5983aca6d8b5c974dead0f8482aab7a97
NazarioSaf <NazarioSaf@yandex.ru>
2022-12-04 20:37:32 +0300

    urok 30

---
+++ src/Redux/State.js
@@ -0,0 +1,36 @@
+
+let state = {
+    profilePage: {
+        post: [
+            { id: 1, message: 'Hi, how are you', like: 12 },
+            { id: 2, message: 'Это мой первый пост?', like: 42 }
+        ]
+    },
+    dialogPage: {
+        dialog: [
+            { id: 1, name: 'Nazar' },
+            { id: 2, name: 'Kate' },
+            { id: 3, name: 'Oleg' },
+            { id: 4, name: 'Vasya' },
+            { id: 5, name: 'Dima' },
+            { id: 6, name: 'Ivan' },
+        ],
+        message: [
+            { id: 1, message: 'Hi' },
+            { id: 2, message: 'How are you?' },
+            { id: 3, message: 'Yeap' },
+            { id: 4, message: 'пока пока' },
+            { id: 5, message: 'бай' },
+        ]
+    },
+    sideBar: {
+        name: [
+            { id: 1, name: 'Иван'},
+            { id: 2, name: 'Петя'},
+            { id: 3, name: 'Саша'}
+        ]
+    }
+};
+
+
+export default state;
